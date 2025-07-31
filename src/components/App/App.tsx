c233878d08f5c4262759bc6ee13487c4917f5714
Андрій <gapeev1979@gmail.com>
2025-07-31 23:35:37 +0300

    SearchBar

---
+++ src/components/App/App.tsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react';
+import css from './App.module.css';
+import SearchBar from '../SearchBar/SearchBar';
+import { Toaster } from 'react-hot-toast';
+
+export default function App() {
+  const handleSearchBar = (data: string) => {
+    // console.log('input - ', data);
+    //     Якщо в результаті запиту масив фільмів порожній, виводьте повідомлення:
+    // No movies found for your request.
+    // Ця перевірка виконується в App при обробці HTTP-запиту.
+    // if () {      console.log('No movies found for your request.');    }
+  };
+
+  return (
+    <>
+      <h1>Hello!</h1>
+      <SearchBar onSubmit={handleSearchBar} />
+      <Toaster />
+      <img className={css.logo} src="/react.svg" alt="react" />
+    </>
+  );
+}
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,47 @@
+import css from './SearchBar.module.css';
+import toast from 'react-hot-toast';
+
+interface SearchBarProps {
+  onSubmit: (value: string) => void;
+}
+
+export default function SearchBar({ onSubmit }: SearchBarProps) {
+  const handleSubmit = (formData: FormData) => {
+    const query = formData.get('query') as string;
+
+    if (query === '') {
+      return toast.error('Please enter your search query..');
+    }
+    console.log(query);
+
+    onSubmit(query);
+  };
+
+  return (
+    <header className={css.header}>
+      <div className={css.container}>
+        <a
+          className={css.link}
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by TMDB
+        </a>
+        <form className={css.form} action={handleSubmit}>
+          <input
+            className={css.input}
+            type="text"
+            name="query"
+            autoComplete="off"
+            placeholder="Search movies..."
+            autoFocus
+          />
+          <button className={css.button} type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}
